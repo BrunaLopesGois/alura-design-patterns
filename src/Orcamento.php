36a743187f8a6555371d6235baa57007dd1bbafa
BrunaLopesGois <bruna-chan@live.com>
2021-03-04 13:44:24 -0300

    state

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -2,10 +2,39 @@
 
 namespace Alura\DesignPattern;
 
+use Alura\DesignPattern\EstadosOrcamento\EmAprovacao;
+use Alura\DesignPattern\EstadosOrcamento\EstadoOrcamento;
+
 class Orcamento
 {
     /** @var int */
     public $quantidadeItens;
     /** @var float */
     public $valor;
+    /** @var EstadoOrcamento */
+    public $estadoAtual;
+
+    public function __construct()
+    {
+        $this->estadoAtual = new EmAprovacao();
+    }
+
+    public function aplicaDescontoExtra()
+    {
+        $this->valor -= $this->estadoAtual->calculaDescontoExtra($this);
+    }
+
+    public function aprova()
+    {
+        $this->estadoAtual->aprova($this);
+    }
+
+    public function reprova()
+    {
+        $this->estadoAtual->reprova($this);
+    }
+    public function finaliza()
+    {
+        $this->estadoAtual->finaliza($this);
+    }
 }
